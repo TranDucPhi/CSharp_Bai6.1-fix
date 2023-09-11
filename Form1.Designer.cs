9b3251f7b1eda0409167721c4f9847beaf901219
ADMIN <ADMIN@LAB1-MAY33>
2023-09-11 09:20:35 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CSharp_Bai6._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtHK1 = new System.Windows.Forms.TextBox();
+            this.txtHK2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtDTB = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.txtHOCLUC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KẾT QUẢ HỌC TẬP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "XẾP LOẠI HỌC LỰC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ĐIỂM HKI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "ĐIỂM HKII";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(93, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ĐIỂM TRUNG BÌNH";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(93, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "KẾT QUẢ";
+            // 
+            // txtHK1
+            // 
+            this.txtHK1.Location = new System.Drawing.Point(374, 107);
+            this.txtHK1.Name = "txtHK1";
+            this.txtHK1.Size = new System.Drawing.Size(314, 20);
+            this.txtHK1.TabIndex = 1;
+            // 
+            // txtHK2
+            // 
+            this.txtHK2.Location = new System.Drawing.Point(374, 174);
+            this.txtHK2.Name = "txtHK2";
+            this.txtHK2.Size = new System.Drawing.Size(314, 20);
+            this.txtHK2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(374, 233);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(314, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // txtDTB
+            // 
+            this.txtDTB.Location = new System.Drawing.Point(374, 232);
+            this.txtDTB.Name = "txtDTB";
+            this.txtDTB.Size = new System.Drawing.Size(314, 20);
+            this.txtDTB.TabIndex = 1;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(374, 291);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(314, 20);
+            this.txtKQ.TabIndex = 1;
+            // 
+            // txtHOCLUC
+            // 
+            this.txtHOCLUC.Location = new System.Drawing.Point(374, 355);
+            this.txtHOCLUC.Name = "txtHOCLUC";
+            this.txtHOCLUC.Size = new System.Drawing.Size(314, 20);
+            this.txtHOCLUC.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(306, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "XEM KẾT QUẢ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtHOCLUC);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtDTB);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtHK2);
+            this.Controls.Add(this.txtHK1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BẢNG ĐIỂM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtHK1;
+        private System.Windows.Forms.TextBox txtHK2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtDTB;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.TextBox txtHOCLUC;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Bai6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hk1, hk2, dtb;
+            hk1 = Convert.ToDouble(txtHK1.Text);
+            hk2 = Convert.ToDouble(txtHK2.Text);
+            dtb = (hk1 + hk2) / 2;
+            txtDTB.Text = Convert.ToString(dtb);
+            if (dtb >= 5)
+            {
+                txtKQ.Text = "ĐƯỢC LÊN LỚP";
+            }
+            else 
+            {
+                txtKQ.Text = "Ở LẠI LỚP";
+            }
+            if (dtb < 5) 
+            {
+                txtHOCLUC.Text = "Yếu";
+            }
+            else if (dtb >=5 && dtb < 7) 
+            {
+                txtHOCLUC.Text = "Trung Bình";
+            }
+            else if (dtb >=8 ) 
+            {
+                txtHOCLUC.Text = "Giỏi";
+            }
+        }
+    }
+}
